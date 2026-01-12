efa801b6402055d30949a3a077aa02e2fed155c6
Marten Biehl <marten.biehl@co-axial.studio>
2026-01-12 11:04:26 +0100

    trying to recover files

---
+++ compress_video.fish
@@ -0,0 +1,5 @@
+#!/opt/homebrew/bin/fish
+for file in (find . -type f -name "*.mov")
+    set output (string replace -r '\.mov$' '_c.mov' $file)
+    ffmpeg -i $file -c:v libx264 -crf 23 -c:a aac $output
+end
