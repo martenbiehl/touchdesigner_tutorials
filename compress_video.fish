b1f139dc9e8429f0a28633511b889f772e43ecd9
Marten Biehl <marten.biehl@co-axial.studio>
2026-01-12 11:17:17 +0100

    dont convert videos that were already converted

--- compress_video.fish
+++ compress_video.fish
@@ -1,5 +1,5 @@
 #!/opt/homebrew/bin/fish
-for file in (find . -type f -name "*.mov")
+for file in (find . -type f -name "*.mov" -not -name "*_c.mov")
     set output (string replace -r '\.mov$' '_c.mov' $file)
     ffmpeg -i $file -c:v libx264 -crf 23 -c:a aac $output
 end
